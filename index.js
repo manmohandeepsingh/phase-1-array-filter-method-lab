538d75b34dc63e1ab85fb9cb28beb306c809474e
Manmohandeep Singh <ltsbcsgo@gmail.com>
2023-01-01 03:07:18 -0500

    commit tree bd128d5a181f9d5ece7ae7c39958c1bdee25d788

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, string) {
+return drivers.filter(driver => driver.toLowerCase() === string.toLowerCase());
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(driver => driver.toLowerCase().indexOf(string.toLowerCase()) === 0);
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(drivers => drivers.name === string)
+}
